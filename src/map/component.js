785765d7fd23bf3ce1f7acd8c955edfdb9f62707
Unknown <rodionaaafgh@gmail.com>
2017-04-28 05:16:05 +0300

    refactoring of map component

--- src/map/component.js
+++ src/map/component.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import './styles.css'
 
-
-const Map = withGoogleMap(props => (
+const PreMap = withGoogleMap(props => (
   <GoogleMap
     disableDefaultUI={true}
-    scaleControl={false}s
+    scaleControl={false}
     defaultZoom={15}
     defaultCenter={{ lat: 53.9045, lng: 27.5615 }}
     onClick={props.onMapClick}
   ></GoogleMap>
 ));
 
-
-export default () => (
-    <Map
+const Map = () => (
+    <PreMap
         containerElement={
-        <div style={{ height: `100%` }} />
+          <div style={{ height: `100%` }} />
         }
         mapElement={
-        <div style={{ height: `100%` }} />
+          <div style={{ height: `100%` }} />
         }
         onMapLoad={console.log('onMapLoad')}
         onMapClick={console.log('onMapClick')}
-        markers={[]}
         onMarkerRightClick={console.log('onMarkerRightClick')}
-    />
+    ></PreMap>
+)
+
+export default () => (
+  <div className="map">
+    <h2>Map</h2>
+    <Map />
+  </div>
 )
