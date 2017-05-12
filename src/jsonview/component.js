6661b62b96f4c1be20825744ed92a0b9df79df42
Unknown <rodionaaafgh@gmail.com>
2017-05-12 06:54:56 +0300

    computing coordinates relatively to first node of the platform

--- src/jsonview/component.js
+++ src/jsonview/component.js
@@ -1,17 +1,48 @@
 import React from 'react'
+const m = window.google.maps
 
+import {BuildingUC} from '../models/construction'
+import type {Nodes} from '../models/common'
+import {Node} from '../models/common'
+import {statePropertyChangeListener} from '../utils/common'
+import {map} from '../map/global'
+import * as u from '../utils/map'
 import './styles.css'
+
 
 const prettyJson = (obj) => JSON.stringify(obj, null, 2)
 
 type Props = {
-    value: any,
+    buildingUC: BuildingUC,
+    enters: Array<Nodes>,
+    exits: Array<Nodes>,
+    platform: Nodes,
     completed: boolean,
 }
 
-export default (props: Props) => (
-    <div hidden={!props.completed} className={'jsonview'}>
-        <h2>JSON output</h2>
-        <div><pre>{prettyJson(props.value)}</pre></div>
-    </div>
-)
+export default class JsonView extends React.Component<void, Props, void> {
+    constructor(props: Props) {
+        super(props)
+        statePropertyChangeListener('map.completed', this.computeCoordinates)
+    }
+
+    render() {
+        return (
+            <div hidden={!this.props.completed} className={'jsonview'}>
+                <h2>JSON output</h2>
+                <div><pre>{true}</pre></div>
+            </div>
+        )
+    }
+
+    computeCoordinates = () => {
+        const p = this.props.platform
+        const db = m.geometry.spherical.computeDistanceBetween
+        const origin = p[0]
+        p.slice(1).forEach(n => {
+            const yDiff = db(origin.googleLatLng(), new Node(origin.lat, n.lon).googleLatLng())
+            const xDiff = db(origin.googleLatLng(), new Node(n.lat, origin.lon).googleLatLng())
+            console.info(xDiff, yDiff)
+        })
+    }
+}
--- src/manager/shaper/component.js
+++ src/manager/shaper/component.js
@@ -8,7 +8,7 @@ type State = {
     setBuildingUC: Function,
 }
 
-export default class Shaper extends React.Component<void, State, void> {
+export default class Shaper extends React.Component<void, void, State> {
     render = () => (
         <h1>Shaper</h1>
     )
